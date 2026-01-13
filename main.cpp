cf5e378ed6595af8f868b4e5b98ce060edd083a4
sashi <satoshi3104.nishi@gmail.com>
2026-01-13 13:25:49 +0900

    ppm 出力

--- main.cpp
+++ main.cpp
@@ -2,6 +2,40 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#ifdef _WIN32
+    if (freopen("../output.ppm", "w", stdout) == nullptr)
+    {
+        std::cerr << "Failed to redirect stdout\n";
+        return 1;
+    }
+#endif
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++)
+    {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
+        for (int i = 0; i < image_width; i++)
+        {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
 }
